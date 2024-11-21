de33ae8b584cd136c86ab09b22f01ae62d2bfbe3
Renan Andrei <105610327+RenanAndrei@users.noreply.github.com>
2024-11-21 20:32:33 -0300

    sa

--- src/js/main.js
+++ src/js/main.js
@@ -17,5 +17,11 @@ $(window).on('scroll', function(){
     header.outerHeight();
 
     let activeSectionIndex = 0;
-        
-})
+    
+    if (scrollPosition <= 0) {
+        header.css('box-shadow', 'none');
+    } else {
+        header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0.1');
+
+    }
+});
